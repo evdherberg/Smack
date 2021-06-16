08c061aea3c5c5cb892ea809dbba5d8dbbb8bec4
Dan Caseley <dan@caseley.me.uk>
2021-06-16 15:22:20 +0100

    [sinttest] Additional tests for s6 of XEP-0045

--- smack-integration-test/src/main/java/org/jivesoftware/smackx/muc/AbstractMultiUserChatIntegrationTest.java
+++ smack-integration-test/src/main/java/org/jivesoftware/smackx/muc/AbstractMultiUserChatIntegrationTest.java
@@ -122,4 +122,12 @@ public class AbstractMultiUserChatIntegrationTest extends AbstractSmackIntegrati
         answerForm.setAnswer("muc#roomconfig_moderatedroom", true); //TODO Add this to the MucConfigFormManager?
         muc.sendConfigurationForm(answerForm);
     }
+
+    static void createHiddenMuc(MultiUserChat muc, String resourceName) throws SmackException.NoResponseException, XMPPException.XMPPErrorException, InterruptedException, MultiUserChatException.MucAlreadyJoinedException, SmackException.NotConnectedException, MultiUserChatException.MissingMucCreationAcknowledgeException, MultiUserChatException.NotAMucServiceException, XmppStringprepException {
+        muc.create(Resourcepart.from(resourceName));
+        Form configForm = muc.getConfigurationForm();
+        FillableForm answerForm = configForm.getFillableForm();
+        answerForm.setAnswer("muc#roomconfig_publicroom", false); //TODO Add this to the MucConfigFormManager?
+        muc.sendConfigurationForm(answerForm);
+    }
 }
